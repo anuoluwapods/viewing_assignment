70be58cb5a85c6fa6d1419ca2ddc410f5fe8dc7a
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2023-08-08 19:23:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from deta import Deta
 import streamlit as st
 
 # Initialize Deta instance
-deta = Deta(st.secrets["deta_key"]) # Replace with your Deta project key
+deta = Deta('YOUR_PROJECT_KEY')  # Replace with your Deta project key
 user_db = deta.Base('assignment_collection')
 user_drive = deta.Drive('assignment')
 
@@ -13,29 +13,34 @@ def main():
     # Fetch user data from the database
     user_data_list = user_db.fetch()
 
-    # Display user information
+    # Filters for cohort and course type
+    selected_cohort = st.sidebar.selectbox("Select Cohort", ["All"] + list(set(user_data["cohort"] for user_data in user_data_list)))
+    selected_course_type = st.sidebar.selectbox("Select Course Type", ["All"] + list(set(user_data["course_type"] for user_data in user_data_list)))
+
+    # Display user information based on filters
     for user_data in user_data_list:
-        st.write("Name:", user_data.get("name"))
-        st.write("Email:", user_data.get("email"))
-        st.write("Cohort:", user_data.get("cohort"))
-        st.write("Course Type:", user_data.get("course_type"))
+        if (selected_cohort == "All" or user_data["cohort"] == selected_cohort) and (selected_course_type == "All" or user_data["course_type"] == selected_course_type):
+            st.write("Name:", user_data.get("name"))
+            st.write("Email:", user_data.get("email"))
+            st.write("Cohort:", user_data.get("cohort"))
+            st.write("Course Type:", user_data.get("course_type"))
 
-        # Check if the user has a linked file
-        if "file_name" in user_data and "file_url" in user_data:
-            st.write("Linked File:", user_data["file_name"])
-            st.write("File URL:", user_data["file_url"])
+            # Check if the user has a linked file
+            if "file_name" in user_data and "file_url" in user_data:
+                st.write("Linked File:", user_data["file_name"])
+                st.write("File URL:", user_data["file_url"])
 
-            # Download and display the linked file
-            file_name = user_data["file_name"]
-            file = user_drive.get(file_name)
-            st.write("File Content:")
-            st.download_button(
-                label=f"Download {file_name}",
-                data=file.content,
-                file_name=file_name
-            )
+                # Download and display the linked file
+                file_name = user_data["file_name"]
+                file = user_drive.get(file_name)
+                st.write("File Content:")
+                st.download_button(
+                    label=f"Download {file_name}",
+                    data=file.content,
+                    file_name=file_name
+                )
 
-        st.write("---")  # Divider between user entries
+            st.write("---")  # Divider between user entries
 
 if __name__ == "__main__":
     main()
